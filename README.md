e4588b5f20a239fe5d99751ff056789eed6f13af
Akhil <akhilenderb9@gmail.com>
2023-08-06 13:39:07 +0530

    made code-validator

---
+++ README.md
@@ -0,0 +1,33 @@
+# code-executer
+
+> A GitHub App built with [Probot](https://github.com/probot/probot) that A Probot app
+
+## Setup
+
+```sh
+# Install dependencies
+npm install
+
+# Run the bot
+npm start
+```
+
+## Docker
+
+```sh
+# 1. Build container
+docker build -t code-executer .
+
+# 2. Start container
+docker run -e APP_ID=<app-id> -e PRIVATE_KEY=<pem-value> code-executer
+```
+
+## Contributing
+
+If you have suggestions for how code-executer could be improved, or want to report a bug, open an issue! We'd love all and any contributions.
+
+For more, check out the [Contributing Guide](CONTRIBUTING.md).
+
+## License
+
+[ISC](LICENSE) © 2023 Akhil
---
+++ index.js
@@ -0,0 +1,54 @@
+/**
+ * This is the main entrypoint to your Probot app
+ * @param {import('probot').Probot} app
+ */
+const axios = require('axios');
+module.exports = (app) => {
+  // Your code here
+  app.log.info("Yay, the app was loaded!");
+
+  app.on("issues.opened", async (context) => {
+    app.log.info(context.payload.issue.body);
+    const myCode = context.payload.issue.body;
+    try {
+      const requirements = {
+        language: "python",
+        version: "3.10.0",
+        files: [
+          {
+            name: "my_cool_code.py",
+            content: myCode
+          },
+        ],
+      };
+      const response = await fetch('https://emkc.org/api/v2/piston/execute', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(requirements),
+      });
+
+      if (!response.ok) {
+        throw new Error(`API request failed with status ${response.status}`);
+      }
+
+      const responseData = await response.json();
+      const output = responseData.run.output;
+      app.log.info('Output:', output);
+
+      const issueComment = context.issue({
+        body: `Output of the above python code : ${output}`,
+      });
+      return context.octokit.issues.createComment(issueComment);
+
+    } catch (error) {
+      app.log.error("error")
+      const issueComment = context.issue({
+        body: "Error in executing the python code!!",
+      });
+      return context.octokit.issues.createComment(issueComment);
+    }
+  });
+
+};
